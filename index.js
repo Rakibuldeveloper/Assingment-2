c3ac2061de3abfc122377a87848919643092de0e
Rakibuldeveloper <morfiverr@gmail.com>
2024-06-25 20:14:36 +0600

    thik ase to

---
+++ index.js
@@ -0,0 +1,10 @@
+                            // Ans to the question no 1
+
+function calculateSum(num1, num2) {
+    
+    const sum = num1 + num2;
+    return sum;
+}
+
+const result = calculateSum(5, 5);
+console.log(result);

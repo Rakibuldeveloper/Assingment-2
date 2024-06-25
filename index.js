46f275ee1b135fb4c67c24135d96dde6ce9e3377
Rakibuldeveloper <morfiverr@gmail.com>
2024-06-25 21:02:57 +0600

    just like wow

--- index.js
+++ index.js
@@ -7,4 +7,27 @@ function calculateSum(num1, num2) {
 }
 
 const result = calculateSum(5, 5);
-console.log(result);
+console.log('calculate sum',result);
+
+
+
+                            //   Ans to the question no 2
+
+
+function isEven (evenorodd){
+    const check = evenorodd % 2 === 0;
+    return check;
+}
+console.log('This is even',isEven(2));
+console.log('This is odd',isEven(3));
+
+
+                        //    Ans to the qusetion no 3
+
+function findMax(maxNumber) {
+    const max = Math.max(...maxNumber);
+    return max;
+}
+
+const arr = [20, 34, 35, 69, 24, 10, 100];
+console.log('The largest number is',findMax(arr));
